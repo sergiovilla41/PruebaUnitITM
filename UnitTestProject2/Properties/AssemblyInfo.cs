e3b35db76f4da5b88458742e26abe9ae3485e7bb
Sergio <serfusion41@gmail.com>
2023-11-06 02:02:42 -0500

    Agregar archivos de proyecto.

---
+++ UnitTestProject2/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestProject2")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestProject2")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("9dbcbbdc-47e2-4fdc-8c36-59aa83aee435")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTestProject2/UnitTest1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.IE;
+using System.Threading;
+
+namespace SeleniumBingTests
+{
+    /// <summary>
+    /// Summary description for MySeleniumTests
+    /// </summary>
+    [TestClass]
+    public class MySeleniumTests
+    {
+        private TestContext testContextInstance;
+        private IWebDriver driver;
+        private string appURL;
+
+        public MySeleniumTests()
+        {
+        }
+
+        [TestMethod]
+        [TestCategory("Chrome")]
+        public void TheBingSearchTest()
+        {
+            driver.Navigate().GoToUrl(appURL + "/");
+            driver.FindElement(By.Name("username")).SendKeys("sergiogallego307270@correo.itm.edu.co");
+            driver.FindElement(By.Name("username")).Click();
+            driver.FindElement(By.Name("password")).SendKeys("1035915351");
+            driver.FindElement(By.Name("password")).Click();
+            Thread.Sleep(4000);
+            IWebElement botonAcceder = driver.FindElement(By.XPath("//button[contains(text(), 'Acceder')]"));
+            botonAcceder.Click();
+            driver.FindElement(By.XPath("//ol[@id='b_results']/li/h2/a/strong[3]")).Click();
+            Assert.IsTrue(driver.Title.Contains("Azure Pipelines"), "Verified title of the page");
+        }
+
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        [TestInitialize()]
+        public void SetupTest()
+        {
+            appURL = "https://campus.arrobamedellin.edu.co";
+
+            string browser = "Chrome";
+            switch (browser)
+            {
+                case "Chrome":
+                    driver = new ChromeDriver();
+                    break;
+                case "Firefox":
+                    driver = new FirefoxDriver();
+                    break;
+                case "IE":
+                    driver = new InternetExplorerDriver();
+                    break;
+                default:
+                    driver = new ChromeDriver();
+                    break;
+            }
+
+        }
+
+        [TestCleanup()]
+        public void MyTestCleanup()
+        {
+            Thread.Sleep(5000);
+            driver.Quit();
+        }
+    }
+}
